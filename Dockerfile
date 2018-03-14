ec584b4143804df50b401cd0fb7c8b0d49e2a6db
Nelson <nelson@sensay.it>
2018-03-14 10:48:15 -0700

    adding dig

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ruby:2.1-alpine
+FROM ruby:2.2.9-alpine
 RUN gem install sinatra --no-doc
-COPY pleasewait.rb .
-CMD ["ruby","pleasewait.rb"]
+COPY pleasewait.rb /usr/src/pleasewait.rb
+CMD ["/usr/local/bin/ruby","/usr/src/pleasewait.rb"]
--- pleasewait.rb
+++ pleasewait.rb
@@ -1,5 +1,7 @@
 # pleasewaite.rb
 require 'sinatra'
+require 'resolv'
+
 set :bind, '0.0.0.0'
 
 get '/' do
@@ -7,11 +9,16 @@ get '/' do
 end
 
 get '/wait/:waittime' do |waittime|
-  if waittime.to_i > 60000
-   return "Wait time exceeds typical http request time of 60 seconds. Please lower wait time."
+  if waittime.to_i > 120000
+   return "Wait time exceeds typical http request time of 120 seconds. Please lower wait time."
   else
     wait_seconds = "#{waittime}".to_i/1000.to_f
     sleep(wait_seconds)
     return "#{wait_seconds} seconds" 
   end
+end
+
+get '/dig/:addr' do |addr|
+  ips = Resolv.getaddresses "#{addr}"
+  return "#{ips}"
 end
